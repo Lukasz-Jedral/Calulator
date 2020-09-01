1db205fb2813c1794d686d2d496d6fde0f768da7
Lukasz Jedral <geodezja.jedral@gmail.com>
2020-09-01 17:51:02 +0200

    Fix problem with posting data to app.py. Used request.form.getlist instead of request.form.get.                    Add to result.html templet amount of foreign currency that is being exchenged

--- Currency_Calc/app.py
+++ Currency_Calc/app.py
@@ -11,7 +11,7 @@ def get_currencies_full_names_from_csv(integer):
             currencies.append(row[0])
     return currencies
 
-def get_whole_row_for_chosen_currency_form_csv_file(currency_ful_name):
+def get_whole_row_for_chosen_currency_form_csv_file(currency_full_name):
     with open('resources\Dane.csv', newline='') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -21,19 +21,20 @@ def get_whole_row_for_chosen_currency_form_csv_file(currency_ful_name):
     return f_result
 
 
-@app.route('/currency_calc/main', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def get_main_page():
     if request.method == 'GET':
         currencies = get_currencies_full_names_from_csv(0)
         return render_template("main.html", currencies=currencies)
 
     elif request.method == 'POST':
-        currency_full_name = request.form['currency']
-        currency_amount = request.form.get('HowMuchCurr')
+        posted_list = request.form.getlist('HowMuchCurr')
+        currency_full_name = posted_list[0]
+        currency_amount = posted_list[1]
         currency_row = get_whole_row_for_chosen_currency_form_csv_file(currency_full_name)
         result = float(currency_amount) * float(currency_row[3])
         currency_code = currency_row[1]
-        return render_template("result.html", choosen_currency_code = currency_code, calc_result = result)
+        return render_template("result.html", currency_amount = currency_amount, chosen_currency_code = currency_code, calc_result = result)
 
 if __name__=="__main__":
     app.run()
